94e38b54105f8fcb07a53bcad94839b8813ad4a9
ant <devnull@localhost>
2010-02-15 11:24:40 +0300

    Added objectuniquepool

---
+++ src/ru/iitp/proling/common/UniqueObjectPool.java
@@ -0,0 +1,48 @@
+package ru.iitp.proling.common;
+
+import java.util.HashMap;
+
+/**
+ * Unique hash set class. Used for storing and retrieving elements
+ * from this set.
+ * @author ant
+ *
+ * @param <T>
+ */
+public class UniqueObjectPool<T> {
+	HashMap<T,T> internalMap;
+	
+	/**
+	 * Gets (and adds if absent) object from hash set
+	 * @param object query object
+	 * @return unique object 
+	 */
+	public T addget(T object){
+		if(internalMap.containsKey(object))
+			return internalMap.get(object);
+		
+		internalMap.put(object, object);
+		return object;
+	}
+	
+	/**
+	 * Add object to hash set 
+	 * @param object
+	 * @return true on successful adding, false if object already exists
+	 */
+	public boolean add(T object){
+		if(internalMap.containsKey(object))
+			return false;
+		internalMap.put(object, object);
+		return true;
+	}
+	
+	/**
+	 * Check if hash set contains object
+	 * @param object 
+	 */
+	public boolean contains(T object){
+		return internalMap.containsKey(object);
+	}
+
+}
