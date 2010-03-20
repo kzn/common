ddd688672731a09e4488e56315a29919b7fccd91
ant <devnull@localhost>
2010-03-20 18:24:12 +0300

    added serialization test

---
+++ src/TestAlphabet.java
@@ -0,0 +1,37 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+import ru.iitp.proling.common.Alphabet;
+
+
+public class TestAlphabet {
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws IOException, ClassNotFoundException{
+		Alphabet<String> towrite = new Alphabet<String>("");
+		
+		towrite.get("a");
+		towrite.get("b");
+		
+		FileOutputStream fos = new FileOutputStream("test.dat");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		
+		oos.writeObject(towrite);
+		oos.close();
+		fos.close();
+		
+		Alphabet<String> toRead;
+		
+		FileInputStream fis = new FileInputStream("test.dat");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		toRead = (Alphabet<String>)ois.readObject();
+		
+		assert toRead.get(1).equals("a");
+
+	}
+
+}
