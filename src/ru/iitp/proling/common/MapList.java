8b0c226346aacdd0f670d537d0b50b41bafce50b
Anton Kazennikov <kazennikov@gmail.com>
2011-05-17 10:04:28 +0400

    added MapList interface and implementation

---
+++ src/ru/iitp/proling/common/MapList.java
@@ -0,0 +1,19 @@
+package ru.iitp.proling.common;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public interface MapList<K, V> {
+	
+	public List<V> get(K key);
+	public void put(K key, V value);
+	public int size();
+	
+	public Set<K> keySet();
+	public Collection<List<V>> values();
+	public boolean contains(K key);
+	
+	
+	
+}
