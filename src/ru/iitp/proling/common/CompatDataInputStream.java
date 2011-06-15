394724fd82011be702561e5f5eee3e41d704d7d0
Anton Kazennikov <kzn@iitp.ru>
2011-06-15 13:24:24 +0400

    added javadocs

--- src/ru/iitp/proling/common/CompatDataInputStream.java
+++ src/ru/iitp/proling/common/CompatDataInputStream.java
@@ -5,12 +5,25 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * DataInputStream that supports reading UTF-8 strings from the stream.
+ * Format is: [int length] [utf-8 bytes]
+ * @author ant
+ *
+ */
 public class CompatDataInputStream extends DataInputStream {
 
 	public CompatDataInputStream(InputStream in) {
 		super(in);
 	}
 	
+	
+	/**
+	 * Read string from the stream. Format is [int] [utf-8 bytes]
+	 * @return resulting stream
+	 * @throws IOException
+	 * @throws EOFException
+	 */
 	public String readString() throws IOException, EOFException {
 		int len = readInt();
 		
--- src/ru/iitp/proling/common/CompatDataOutputStream.java
+++ src/ru/iitp/proling/common/CompatDataOutputStream.java
@@ -4,12 +4,25 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * DataOutputStream that supports writing strings in UTF-8 format.
+ * 
+ * Strings are written in this format: [int] [utf-8 bytes], where [int] is an
+ * integer size string length
+ * @author ant
+ *
+ */
 public class CompatDataOutputStream extends DataOutputStream {
 
 	public CompatDataOutputStream(OutputStream out) {
 		super(out);
 	}
 	
+	/**
+	 * Write string to the stream
+	 * @param s string to write
+	 * @throws IOException
+	 */
 	public void writeString(String s) throws IOException {
 		writeInt(s.length());
 		byte[] encoded = s.getBytes("UTF-8");
