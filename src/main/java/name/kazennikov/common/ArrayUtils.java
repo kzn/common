9449cd44fe17118321244ca4013df298ce79ce9d
Anton Kazennikov <kazennikov@gmail.com>
2013-11-15 12:17:22 +0400

    consistent formatting

--- src/main/java/name/kazennikov/common/ArrayUtils.java
+++ src/main/java/name/kazennikov/common/ArrayUtils.java
@@ -2,27 +2,27 @@ package name.kazennikov.common;
 
 public class ArrayUtils {
 	
-	public static void swap(int[] array, int i, int j){
+	public static void swap(int[] array, int i, int j) {
 		int t = array[i];
 		array[i] = array[j];
 		array[j] = t;
 	}
 	
 	
-	public static void swap(double[] array, int i, int j){
+	public static void swap(double[] array, int i, int j) {
 		double t = array[i];
 		array[i] = array[j];
 		array[j] = t;
 	}
 	
-	public static void shuffle(double[] array, int active){
+	public static void shuffle(double[] array, int active) {
 		for(int i = 0; i != active; i++){
 			int j = i + (int)(Math.random() * (active - i));
 			swap(array, i, j);
 		}
 	}
 	
-	public static void shuffle(int[] array, int active){
+	public static void shuffle(int[] array, int active) {
 		for(int i = 0; i != active; i++){
 			int j = i + (int)(Math.random() * (active - i));
 			swap(array, i, j);
