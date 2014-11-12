76f8f9bec8ffe9462d040e1dd537b7e1109ee5fe
Anton Kazennikov <kazennikov@gmail.com>
2014-11-12 10:30:12 +0400

    simple class for counting occurrences

---
+++ src/main/java/name/kazennikov/common/TopCount.java
@@ -0,0 +1,67 @@
+package name.kazennikov.common;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import gnu.trove.map.hash.TObjectIntHashMap;
+import gnu.trove.procedure.TObjectIntProcedure;
+
+public class TopCount<E> {
+	public static class Pair<E> {
+		public final E data;
+		public final int count;
+		
+		public Pair(E data, int count) {
+			super();
+			this.data = data;
+			this.count = count;
+		}
+		
+		
+	}
+	
+	TObjectIntHashMap<E> counts = new TObjectIntHashMap<E>();
+	
+	public void add(E o, int count) {
+		counts.adjustOrPutValue(o, count, count);
+	}
+	
+	public List<Pair<E>> build() {
+		final List<Pair<E>> pairs = new ArrayList<Pair<E>>();
+		
+		counts.forEachEntry(new TObjectIntProcedure<E>() {
+
+			@Override
+			public boolean execute(E a, int b) {
+				pairs.add(new Pair<E>(a, b));
+				return true;
+			}
+		});
+		
+		Collections.sort(pairs, new Comparator<Pair<E>>() {
+
+			@Override
+			public int compare(Pair<E> o1, Pair<E> o2) {
+				return o2.count - o1.count;
+			}
+		});
+		
+		return pairs;
+	}
+	
+	
+	public void write(File f) throws IOException {
+		try(PrintWriter pw = new PrintWriter(f, "UTF-8")) {
+			for(Pair<E> pair : build()) {
+				pw.printf("%s\t%d%n", pair.data, pair.count);
+			}
+		}
+	}
+	
+
+}
