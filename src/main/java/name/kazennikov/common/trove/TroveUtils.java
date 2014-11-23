952c94902ad76c41dc92d7373a35dc5f5a513c9f
Anton Kazennikov <kazennikov@gmail.com>
2014-11-23 21:46:20 +0400

    adding utils from fsa

--- src/main/java/name/kazennikov/common/trove/TroveUtils.java
+++ src/main/java/name/kazennikov/common/trove/TroveUtils.java
@@ -6,6 +6,8 @@ import gnu.trove.list.TDoubleList;
 import gnu.trove.list.TFloatList;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.TShortList;
+import gnu.trove.list.array.TCharArrayList;
+import gnu.trove.list.array.TIntArrayList;
 
 public class TroveUtils {
 		private TroveUtils() {}
@@ -44,6 +46,37 @@ public class TroveUtils {
 			byte t = l.get(i);
 			l.set(i, l.get(j));
 			l.set(j, t);
+		}
+		
+		public static void expand(TIntArrayList dest, CharSequence s) {
+			dest.resetQuick();
+			
+			for(int i = 0; i < s.length(); i++) {
+				dest.add(s.charAt(i));
+			}
+		}
+		
+		public static void expand(TCharArrayList dest, CharSequence s) {
+			dest.resetQuick();
+			
+			for(int i = 0; i < s.length(); i++) {
+				dest.add(s.charAt(i));
+			}
+		}
+		
+		
+		
+		public static void expand(TIntArrayList dest, CharSequence from, CharSequence to) {
+			dest.resetQuick();
+			int maxLength = Math.max(from.length(), to.length());
+			
+			for(int i = 0; i < maxLength; i++) {
+				char wfChar = i < from.length()? from.charAt(i) : 0;
+				char lemmaChar = i < to.length()? to.charAt(i) : 0;
+				
+				int destSymbol = (lemmaChar << 16) + wfChar;
+				dest.add(destSymbol);
+			}
 		}
 
 
